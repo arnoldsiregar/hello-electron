b8bbba1629dc418274f6ffd30ecdec3e0d33ce00
Arnold Siregar <arnold.siregar@gmail.com>
2019-06-26 11:21:46 +0700

    add comments

--- main.js
+++ main.js
@@ -4,10 +4,13 @@ const path = require('path');           // node
 
 const { app, BrowserWindow } = electron;
 
+// Declare main window
 let mainWindow;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow();
+
+    // Load html into mainWindow
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, "index.html"),
         protocol: "file:",
